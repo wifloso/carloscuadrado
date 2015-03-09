3071746f62bc2c0d40ee236904006ab55312a99d
CarlosLobato <carloslobatooficial@gmail.com>
2015-03-09 18:09:00 +0100

    ModificacionAlumno2

--- GitAux.java
+++ GitAux.java
@@ -15,10 +15,13 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+	System.out.println("modificado por alumno 2"); // alumno 2
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+	// modificaciones alumno 2
+	System.out.println("Alumno 2 inserta esta sentencia");
     }
 	public void testGitAux(){
  	// metodo rama testing
