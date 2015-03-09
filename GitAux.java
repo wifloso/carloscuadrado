6e687a960c5365b64b5e22d4bb641d8ae7241bca
wifloso <carlossalte@uma.es>
2015-03-09 18:02:49 +0100

    vuelta atras

--- GitAux.java
+++ GitAux.java
@@ -11,7 +11,6 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
-	System.out.println("modificado por alumno 1"); // alumno 1
     }
     
     public void metodoAlumno2(){
@@ -20,8 +19,6 @@ public class GitAux {
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
-	// modificaciones alumno 1
-	System.out.println("Alumno 1 inserta esta sentencia");	
     }
 	public void testGitAux(){
  	// metodo rama testing
